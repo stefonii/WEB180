f7aaf44c56c7b943ec597f13eabb811cace71fc8
Stephanie <s.nguyen407@gmail.com>
2019-10-16 19:14:30 -0400

    Add files via upload
    Added Lesson 1 folder

---
+++ Lesson 1/acme/HelloWorld/HelloWorld/example.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Acme.Collections;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // From Acme.Collections
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push(10);
+            s.Push(100);
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+        }
+    }
+}
